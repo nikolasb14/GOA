55a0373c6ea5e96cf179845973fc0266c4e88f1a
nikolasb14 <nikolasbobokhidze14@gmail.com>
2024-02-28 22:16:52 +0400

    next

--- JavaScript/day 8/script.js
+++ JavaScript/day 8/script.js
@@ -1,11 +1,55 @@
-const calculator = {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
-    multiply: (a, b) => a * b,
-    divide: (a, b) => a / b
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
   };
 
-  const resultAdd = calculator.add(10, 5); // 15
-const resultSubtract = calculator.subtract(10, 5); // 5
-const resultMultiply = calculator.multiply(10, 5); // 50
-const resultDivide = calculator.divide(10, 5); // 2
+  const personName = person.name;
+console.log(`Person's name: ${personName}`);
+
+person.age = 35; // New age value
+
+person.country = "United States";
+
+person.greet = function() {
+    console.log(`Hello, my name is ${this.name}!`);
+  };
+  
+  // Call the greet method
+  person.greet();
+
+  
+  const person1 = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  
+  const person2 = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  
+  // Compare using === operator
+  const areEqual = person1 === person2;
+  console.log(`Are the objects equal? ${areEqual}`);
+
+  
+  const school = {
+    name: "ABC School",
+    students: [
+      { name: "Alice", age: 12 },
+      { name: "Bob", age: 11 },
+      { name: "Charlie", age: 13 }
+    ]
+  };
+  
+  // Access a student's name and age
+  const studentName = school.students[1].name;
+  const studentAge = school.students[1].age;
+  console.log(`Student: ${studentName}, Age: ${studentAge}`);
+
+  
+  
+
